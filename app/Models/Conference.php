4f333378644a154001f3c5b214c0b8b791822e0f
Thuta Min Thway <thutaminthway@onenex.co>
2024-07-16 16:09:24 +0630

    Select Input

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Region;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,14 @@ class Conference extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'id' => 'integer',
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+        'region' => Region::class,
+        'venue_id' => 'integer',
+    ];
 
     public function venue(): BelongsTo
     {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
         ]);
+
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
