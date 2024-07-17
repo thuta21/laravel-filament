2813067e318cbbff2302b14db4576ac046b78f17
Thuta Min Thway <thutaminthway@onenex.co>
2024-07-17 15:49:31 +0630

    pre-video-updates

--- database/migrations/2024_07_16_081409_create_conference_talk_table.php
+++ database/migrations/2024_07_16_081409_create_conference_talk_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('conference_talk', function (Blueprint $table) {
             $table->foreignId('conference_id');
             $table->foreignId('talk_id');
+
         });
     }
 
