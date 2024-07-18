31862f4d198ad6221ca451abd6c5566ae0a6966f
Thuta Min Thway <thutaminthway@onenex.co>
2024-07-18 11:51:03 +0630

    Using filaments outside of panels

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -4,13 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\TextInput;
 
 class Attendee extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     public function conference(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            Group::make()->columns(2)->schema([
+                TextInput::make('name')
+                    ->required()->maxLength(255),
+                TextInput::make('email')
+                    ->email()->required()->maxLength(255),
+            ])
+        ];
     }
 }
